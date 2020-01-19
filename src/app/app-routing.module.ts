b6ec15395866389249a41569d82b58f425f4cb0e
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2020-01-19 13:03:24 +0530

    ommit two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'next',
+    component: HeaderComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

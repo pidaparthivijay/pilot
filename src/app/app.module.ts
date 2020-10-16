85211bf63022c6add4c86ab14dce0cf9f3c3c36b
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2020-10-16 23:59:46 +0530

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

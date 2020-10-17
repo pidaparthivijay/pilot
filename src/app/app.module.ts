b7bd26937fef07fdcc8128d615916c2041586adb
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2020-10-18 00:49:13 +0530

    before animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './shared/menu/menu.component';
-import { HomeComponent } from './home/home.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ContactComponent } from './contact/contact.component';
-import { ResumeComponent } from './resume/resume.component';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TableModule } from "primeng/table";
+import { TooltipModule } from "primeng/tooltip";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContactComponent } from "./contact/contact.component";
+import { EducationComponent } from "./education/education.component";
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { MenuComponent } from "./shared/menu/menu.component";
+import { SkillsComponent } from "./skills/skills.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,9 +25,10 @@ import { TooltipModule } from 'primeng/tooltip';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     TableModule,
     TooltipModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

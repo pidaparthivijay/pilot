d20813ca90a1b2249a3acd090333920959162e44
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2020-10-21 03:40:03 +0530

    projects without individual skill

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,21 +10,60 @@ export class ProjectsComponent implements OnInit {
 
   data;
   projects: any[] = [];
+  cBdata: { datasets: { data: number[]; backgroundColor: string[]; label: string; }[]; labels: string[]; };
+  ccrData: { datasets: { data: number[]; backgroundColor: string[]; label: string; }[]; labels: string[]; };
+  selfServiceData: { datasets: { data: number[]; backgroundColor: string[]; label: string; }[]; labels: string[]; };
+  hotelTourServicesData: { datasets: { data: number[]; backgroundColor: string[]; label: string; }[]; labels: string[]; };
+  portFolioData: { datasets: { data: number[]; backgroundColor: string[]; label: string; }[]; labels: string[]; };
 
   constructor(private sharedService: SharedService) {
+    this.cBdata = {
+      datasets: [{
+        data: [35, 0, 0, 0, 0, 20, 30, 15, 0, 0],
+        backgroundColor: ["#123C69", "#800080", "#FFCE56", "#4F4A41", "#36A2EB", "#86C232", "#F13C20", "#ffffff", "#000000"],
+        label: 'Cherry Brown Services'
+      }],
+      labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"]
+    };
+    this.ccrData = {
+      datasets: [{
+        data: [22, 20, 0, 0, 10, 10, 18, 15, 5, 0
+        ],
+        backgroundColor: ["#123C69", "#800080", "#FFCE56", "#4F4A41", "#36A2EB", "#86C232", "#F13C20", "#ffffff", "#000000"],
+        label: 'Citi Connectivity Registry Utilization'
+      }],
+      labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"]
+    };
+    this.selfServiceData = {
+      datasets: [{
+        data: [25, 0, 25, 12, 0, 0, 0, 0, 8, 30],
+        backgroundColor: ["#123C69", "#800080", "#FFCE56", "#4F4A41", "#36A2EB", "#86C232", "#F13C20", "#ffffff", "#000000"],
+        label: 'CCR Self Service Utilization'
+      }],
+      labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"]
+    };
+    this.hotelTourServicesData = {
+      datasets: [{
+        data: [20, 0, 25, 15, 0, 0, 0, 0, 15, 25],
+        backgroundColor: ["#123C69", "#800080", "#FFCE56", "#4F4A41", "#36A2EB", "#86C232", "#F13C20", "#ffffff", "#000000"],
+        label: 'My Hotel & Tour Utilization'
+      }],
+      labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"]
+    };
+    this.portFolioData = {
+      datasets: [{
+        data: [0, 0, 0, 0, 0, 0, 0, 15, 30, 55],
+        backgroundColor: ["#123C69", "#800080", "#FFCE56", "#4F4A41", "#36A2EB", "#86C232", "#F13C20", "#ffffff", "#000000"],
+        label: 'My Portfolio'
+      }],
+      labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"]
+    };
+
     this.data = {
       labels: ["Java", "Spring", "Spring Boot", "Spring JPA", "Hibernate", "Oracle", "JSP", "JavaScript,TypeScript,jQuery", "CSS", "Angular"],
       datasets: [
         {
           label: 'Cherry Brown Services',
-          // "#",
-          // "#",
-          // "#",
-          // "#",
-          // "#36A2EB",
-          // "#",
-          // "#F13C20"
-
           borderColor: '#800080',
           pointBackgroundColor: 'rgba(179,181,198,1)',
           pointBorderColor: '#fff',

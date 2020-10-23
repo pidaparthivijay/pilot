e3dda296eb9ce0770f33687650e1555747978fb6
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2020-10-24 03:12:37 +0530

    about done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
+  animations: [
+    trigger("visibilityChanged", [
+      state("show", style({ opacity: 1 })),
+      state("hide", style({ opacity: 0 })),
+      transition("show => hide", animate("1000ms")),
+      transition("hide => show", animate("1000ms")),
+    ]),
+  ],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  visiblityState = "hide";
+  bringUpName() {
+    if (this.visiblityState === "hide") {
+      document.getElementById("fullName").style.display = "inline";
+      this.visiblityState = "show";
+    } else {
+      this.visiblityState = "hide";
+      setTimeout(function () {
+        document.getElementById("fullName").style.display = "none";
+      }, 500);
+    }
   }
-
+  ngOnInit(): void {}
 }
